a4c2080f4f265150f7e94218a6594cb8f4ac85fd
BinaryFuck <60218247+BinaryFuck@users.noreply.github.com>
2020-12-13 02:39:59 +0500

    Update file_dir_chk.py

--- file_dir_chk.py
+++ file_dir_chk.py
@@ -1,5 +1,5 @@
 import os
 
 all = 'paste Path '
-print([name for name in os.listdir(all) if os.path.isdir(name)])
-print([name for name in os.listdir(all) if os.path.isfile(name)])
+print([name for name in os.listdir(all) if os.path.isdir(os.path.join(all,name))])
+print([name for name in os.listdir(all) if os.path.isfile(os.path.join(all, name))])
